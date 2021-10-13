48cbf89bc30895b67199c5f74ea872a506be5065
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-10-13 18:06:40 +0800

    first init

---
+++ rust-ffmpeg/script/build.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+rm -rf ../ff-output
+
+cd ../source/FFmpeg
+
+./configure \
+--prefix=../../ff-output \
+--disable-everything \
+--disable-all \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-x86asm \
+--disable-programs \
+--disable-asm \
+--disable-doc \
+--disable-autodetect \
+--disable-optimizations \
+--disable-devices \
+--disable-pthreads \
+--disable-w32threads \
+--disable-network \
+--disable-hwaccels \
+--disable-parsers \
+--disable-bsfs \
+--disable-debug \
+--disable-stripping \
+--disable-protocols \
+--disable-indevs \
+--disable-outdevs \
+--disable-swresample \
+--disable-x86asm \
+–disable-shared \
+--disable-ffplay \
+--disable-hwaccels \
+--enable-protocol=file \
+--enable-protocol=rtmp \
+--enable-decoder=aac \
+--enable-demuxer=mov \
+--enable-demuxer=flv \
+--enable-demuxer=rtsp \
+--enable-demuxer=mp3 \
+--enable-demuxer=h264 \
+--enable-demuxer=aac \
+--enable-muxer=mp4 \
+--enable-demuxer=mp4 \
+--enable-muxer=flv \
+--enable-muxer=h264 \
+--enable-muxer=adts \
+--enable-muxer=mp3 \
+--enable-decoder=aac \
+--enable-decoder=mp3 \
+--enable-decoder=opus \
+--enable-decoder=h264 \
+--enable-decoder=hevc \
+--enable-encoder=aac \
+--enable-encoder=libx264 \
+--enable-demuxer=ogg \
+--enable-demuxer=matroska \
+--enable-demuxer=mov \
+--enable-demuxer=mpegts \
+--enable-demuxer=flv \
+--enable-demuxer=avi \
+--enable-muxer=mpegts \
+--enable-muxer=avi \
+--enable-parser=aac \
+--enable-encoder=jmpeg \
+--enable-decoder=jmpeg \
+--arch=x86_64 \
+--target-os=darwin 
+
+make -j 12
+
+make install
+
+cd -
--- rust-ffmpeg/src/lib.rs
+++ rust-ffmpeg/src/lib.rs
@@ -44,10 +44,21 @@ mod tests {
         .status()
         .expect("source/FFmpeg 下执行 git pull 失败");
     }
-    // 尝试编译 FFmpeg 根据当前 OS 和 arch 
+    // 尝试编译 FFmpeg 根据当前 OS 和 arch
     let complier_lib_path = path_resolve("/ff-output".into());
     if !Path::new(&complier_lib_path).exists() {
-
+      let mut task_complie_ffmpeg_lib = Command::new("sh");
+      if cfg!(target_arch = "arm64") {
+        task_complie_ffmpeg_lib.arg("clone").arg("./build.arm.sh");
+      } else {
+        task_complie_ffmpeg_lib.arg("clone").arg("./build.sh");
+      }
+      let complie_cwd_path = "script";
+      let complie_cwd = path_resolve(complie_cwd_path.into());
+      task_complie_ffmpeg_lib.current_dir(complie_cwd.clone());
+      task_complie_ffmpeg_lib
+        .status()
+        .expect("执行 ffmpeg 构建编译失败!");
     }
   }
 }
