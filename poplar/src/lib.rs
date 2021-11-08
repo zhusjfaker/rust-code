367c64f40aa0cf13ac4486d5356b68bceed9f520
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-11-08 18:21:07 +0800

    fix: 1. code bug

--- poplar/src/lib.rs
+++ poplar/src/lib.rs
@@ -16,22 +16,22 @@ mod tests {
         println!("{}", ffmpeglib::AV_CPU_FLAG_SSE);
         unsafe {
             ffmpeglib::avdevice_register_all();
-            // let path = "/Users/zhushijie/Desktop/m3u8-demo/test.mp4";
-            // let c_path = ffi::CString::new(path)
-            //     .expect("CString::new failed")
-            //     .into_raw();
-            // let txt_inputformat = ffi::CString::new("video4linux2")
-            //     .expect("CString::new failed")
-            //     .into_raw();
-            // let input_fmt = ffmpeglib::av_find_input_format(txt_inputformat);
-            // // let audio_stream_idx = -1;
-            // let mut ifmt_ctx: *mut ffmpeglib::AVFormatContext = ffmpeglib::avformat_alloc_context();
-            // let code = ffmpeglib::avformat_open_input(&mut ifmt_ctx, c_path, input_fmt, null_mut());
-            // if code < 0 {
-            //     println!("找不到视频文件");
-            // } else {
-            //     println!("视频打开成功");
-            // }
+            let path = "/Users/zhushijie/Desktop/m3u8-demo/test.mp4";
+            let c_path = ffi::CString::new(path)
+                .expect("CString::new failed")
+                .into_raw();
+            let txt_inputformat = ffi::CString::new("video4linux2")
+                .expect("CString::new failed")
+                .into_raw();
+            let input_fmt = ffmpeglib::av_find_input_format(txt_inputformat);
+            // let audio_stream_idx = -1;
+            let mut ifmt_ctx: *mut ffmpeglib::AVFormatContext = ffmpeglib::avformat_alloc_context();
+            let code = ffmpeglib::avformat_open_input(&mut ifmt_ctx, c_path, input_fmt, null_mut());
+            if code < 0 {
+                println!("找不到视频文件");
+            } else {
+                println!("视频打开成功");
+            }
             println!("...");
         };
     }
