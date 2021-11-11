bf83d9565b56ecb3f556cd278d742cdd12321bc6
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-11-11 20:34:18 +0800

    fix: 1. code bug

--- poplar/src/lib.rs
+++ poplar/src/lib.rs
@@ -1,10 +1,15 @@
+mod save;
+
 extern crate rust_ffmpeg;
 
 #[cfg(test)]
 mod tests {
     use std::ffi;
     use std::ptr::null_mut;
+    use std::slice::from_raw_parts;
     use rust_ffmpeg::gen::ffmpeg::root as ffmpeglib;
+    use rust_ffmpeg::gen::ffmpeg::root::{AV_TIME_BASE, AVCodecParameters, AVMediaType_AVMEDIA_TYPE_VIDEO};
+    use crate::save::saveframe;
 
     #[test]
     fn it_works() {
@@ -32,7 +37,119 @@ mod tests {
             } else {
                 println!("视频打开成功");
             }
-            println!("...");
+            let value = (*ifmt_ctx).duration;
+            let trade = AV_TIME_BASE as i64;
+            let durtime = value / trade;
+            let ctx = *ifmt_ctx;
+            println!("res is {},time is {}", code, durtime);
+            println!("stream is {}", ctx.nb_streams);
+            let stream_count = ctx.nb_streams;
+            let streams = from_raw_parts((*ifmt_ctx).streams, stream_count as usize)
+                .iter()
+                .map(|x| (*x).as_ref().expect("not null"))
+                .collect::<Vec<&ffmpeglib::AVStream>>();
+
+            for (index, stream_ptr) in streams.iter().enumerate() {
+                let acc: *mut AVCodecParameters = stream_ptr.codecpar;
+                println!("codec_type is {},index is {}", (*acc).codec_type, index);
+                if (*acc).codec_type == AVMediaType_AVMEDIA_TYPE_VIDEO {
+                    video_stream_idx.push(index);
+                    let codec: *const ffmpeglib::AVCodec = ffmpeglib::avcodec_find_decoder((*acc).codec_id);
+                    if codec == null_mut() {
+                        println!("没有该类型的解码器!");
+                        break;
+                    }
+                    let codec_ctx: *mut ffmpeglib::AVCodecContext = ffmpeglib::avcodec_alloc_context3(codec);
+                    ffmpeglib::avcodec_parameters_to_context(codec_ctx, acc);
+                    let res = ffmpeglib::avcodec_open2(codec_ctx, codec, null_mut());
+                    if res != 0 {}
+                    println!("解码器打开成功");
+                    let packet: *mut ffmpeglib::AVPacket = ffmpeglib::av_packet_alloc();
+                    let pframe: *mut ffmpeglib::AVFrame = ffmpeglib::av_frame_alloc();
+                    let tr_frame: *mut ffmpeglib::AVFrame = ffmpeglib::av_frame_alloc();
+
+                    let picturesize = ffmpeglib::avpicture_get_size(
+                        ffmpeglib::AVPixelFormat_AV_PIX_FMT_YUVJ420P,
+                        (*codec_ctx).width,
+                        (*codec_ctx).height,
+                    ) as usize;
+
+                    let buffer = ffmpeglib::av_malloc(picturesize as u64) as *mut u8;
+
+                    ffmpeglib::avpicture_fill(
+                        tr_frame as *mut ffmpeglib::AVPicture,
+                        buffer,
+                        ffmpeglib::AVPixelFormat_AV_PIX_FMT_YUVJ420P,
+                        (*codec_ctx).width,
+                        (*codec_ctx).height,
+                    );
+
+                    println!(
+                        "width: {} height: {} pix_fmt: {}",
+                        (*codec_ctx).width,
+                        (*codec_ctx).height,
+                        (*codec_ctx).pix_fmt
+                    );
+
+                    let mut pic_index = 1;
+
+                    while ffmpeglib::av_read_frame(ifmt_ctx, packet) >= 0 {
+                        let stream_index = (*packet).stream_index as usize;
+                        if video_stream_idx.contains(&stream_index) {
+                            let ret_send = ffmpeglib::avcodec_send_packet(codec_ctx, packet);
+                            if ret_send < 0 {
+                                println!("发送视频帧失败,跳过");
+                                continue;
+                            }
+                            let receive_ret = ffmpeglib::avcodec_receive_frame(codec_ctx, pframe);
+                            if receive_ret < 0 {
+                                println!("解码获取 视频帧失败,跳过");
+                                continue;
+                            }
+
+                            if (*packet).flags == 1 {
+                                pic_index += 1;
+
+                                if pic_index < 20 {
+                                    let img_convert_ctx: *mut ffmpeglib::SwsContext = ffmpeglib::sws_getContext(
+                                        (*pframe).width,
+                                        (*pframe).height,
+                                        (*pframe).format,
+                                        (*pframe).width,
+                                        (*pframe).height,
+                                        ffmpeglib::AVPixelFormat_AV_PIX_FMT_YUVJ420P,
+                                        ffmpeglib::SWS_BICUBIC as i32,
+                                        null_mut(),
+                                        null_mut(),
+                                        null_mut(),
+                                    );
+
+                                    let h = ffmpeglib::sws_scale(
+                                        img_convert_ctx,
+                                        (*pframe).data.as_ptr() as *mut *const u8,
+                                        (*pframe).linesize.as_ptr(),
+                                        0,
+                                        (*codec_ctx).height,
+                                        (*tr_frame).data.as_ptr(),
+                                        (*tr_frame).linesize.as_ptr(),
+                                    );
+
+                                    println!("重新计算的高端:{}", h);
+
+                                    saveframe(tr_frame, pic_index);
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    ffmpeglib::av_frame_unref(pframe);
+                    ffmpeglib::av_frame_unref(tr_frame);
+                    ffmpeglib::avcodec_close(codec_ctx);
+                    ffmpeglib::avformat_close_input(&mut ifmt_ctx);
+                }
+            }
         };
     }
 }
