fc240a44b7f08fc2ac617735deb13e9d3e4cf69d
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2021-11-19 03:00:00 +0800

    fix: 1. bug fix

--- wasm-test/build.rs
+++ wasm-test/build.rs
@@ -1,5 +1,22 @@
+extern crate bindgen;
+
+use std::collections::HashSet;
 use std::ffi::OsString;
+use std::ops::Deref;
 use std::path::Path;
+
+#[derive(Debug, Clone)]
+struct IgnoreMacros(HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
+}
 
 /**
  * 计算项目的全局目录
@@ -45,8 +62,64 @@ fn build_include_dll(static_libs: Vec<String>) {
     }
 }
 
+/*
+ * 生成定义文件
+ */
+fn generate(headerpath: Vec<&str>, outpath: &str, include_path: Option<Vec<&str>>) {
+    let mut bindings = bindgen::Builder::default()
+        .enable_cxx_namespaces()
+        .generate_inline_functions(true)
+        .derive_default(true);
 
+    bindings = bindings.clang_arg("-arch").clang_arg("arm64");
+
+    for header in headerpath.iter() {
+        bindings = bindings.header(header.to_string());
+    }
+
+    let ignored_macros = IgnoreMacros(HashSet::from_iter(vec![
+        String::from("FP_INFINITE"),
+        String::from("FP_NAN"),
+        String::from("FP_NORMAL"),
+        String::from("FP_SUBNORMAL"),
+        String::from("FP_ZERO"),
+        String::from("IPPORT_RESERVED"),
+    ]));
+
+    if !include_path.is_none() {
+        let include_path_list = include_path.unwrap();
+        for path in include_path_list.iter() {
+            bindings = bindings.clang_arg("-I").clang_arg(path.to_string());
+        }
+    }
+
+    let res = bindings
+        .parse_callbacks(Box::new(ignored_macros.clone()))
+        .layout_tests(false)
+        .rustfmt_bindings(true)
+        .detect_include_paths(true)
+        .generate_comments(true)
+        .generate()
+        .unwrap();
+
+    res.write_to_file(outpath).unwrap();
+}
+
+/*
+ * 构建项目方法
+ */
 fn build() {
+    /** 生成 *.rs 定义文件 */
+    let value_headerpaths = calc_project_path(vec!["clib/index.h"].into_iter().map(|s| s.to_owned()).collect());
+    let headerpaths = value_headerpaths.iter().map(|x| x.deref()).collect::<Vec<&str>>();
+    let value_includepaths = calc_project_path(vec!["clib"].into_iter()
+        .map(|s| s.to_owned())
+        .collect());
+    let includepaths = value_includepaths.iter().map(|x| x.deref()).collect::<Vec<&str>>();
+    generate(headerpaths, path_resolve(String::from("src/clib.rs")).as_str(), Some(includepaths));
+    /*
+     * 编译携带 静态库过程
+     */
     let include_static_paths = vec!["clib"];
     build_include_path(calc_project_path(
         include_static_paths
@@ -54,13 +127,8 @@ fn build() {
             .map(|s| s.to_owned())
             .collect(),
     ));
-    let include_static_libs = vec!["clib/libindex.a"];
-    build_include_dll(calc_project_path(
-        include_static_libs
-            .into_iter()
-            .map(|s| s.to_owned())
-            .collect(),
-    ));
+    let include_static_libs = vec!["index"];
+    build_include_dll(include_static_libs.into_iter().map(|s| s.to_owned()).collect());
     println!("build successful!")
 }
 
--- wasm-test/src/lib.rs
+++ wasm-test/src/lib.rs
@@ -1,9 +1,33 @@
+use wasm_bindgen::prelude::*;
+
+// mod clib;
+
+#[wasm_bindgen]
+pub fn greet(num: i32) -> i32 {
+    unsafe {
+        return 2 + num;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    // use crate::test_add;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+
+    // #[test]
+    // fn clib_test() {
+    //     unsafe {
+    //         let a = test_add(2);
+    //         println!("{} is res", a);
+    //         assert_eq!(a, 4);
+    //     }
+    // }
 }
+
+
 
